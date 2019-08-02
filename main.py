b0847fe1072cb1413a869471fc5bf80c51a23200
stratix <konrad_winnicki@wp.pl>
2019-08-02 18:10:26 +0200

    webSocket example

--- main.py
+++ main.py
@@ -2,10 +2,15 @@
 
 from flask import Flask, render_template
 import datetime
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
 @app.route('/')
 def index():
+    print('get index.html')
     templateData = {
         'title' :   'Hello!',
         'time'  :   datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -13,5 +18,23 @@ def index():
     return render_template('index.html', **templateData)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000, host='192.168.1.123')
+    socketio.run(app, debug=True, port=5000, host='192.168.1.122')
     
+@socketio.on('my event')
+def handle_my_custom_event(json):
+    print('received json: ' + str(json))
+    return 'ok'
+
+@socketio.on('message')
+def handle_message(message):
+    print('received message: ' + message)
+    return 'ok'
+
+@socketio.on('connect')
+def test_connect():
+    #emit('my response', {'data': 'Connected'})
+    print('Client connected')
+
+@socketio.on('disconnect')
+def test_disconnect():
+    print('Client disconnected')
