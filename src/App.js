2fbe0140810d73923c13d28e6d010d2dfdb55eac
stratixx <stratixx@wp.pl>
2019-08-25 19:35:14 +0200

    tabela z przyciskami dodana

--- src/App.js
+++ src/App.js
@@ -5,12 +5,54 @@ import './App.css';
 class App extends React.Component {
 
 
+  buttonInput1() {
+    console.log("button input 1 clicked")
+  }
+  buttonInput2() {
+    console.log("button input 2 clicked")
+  }
+  buttonInput3() {
+    console.log("button input 3 clicked")
+  }
 
   render() {
   return <div className="App">
-      hello
+      
+  <table class="GeneratedTable">
+    <thead>
+      <tr>
+        <th>Input</th>
+        <th>State</th>
+        <th>Output</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>
+          <button onClick={this.buttonInput1}>IN1</button>
+        </td>
+        <td>S1</td>
+        <td>OUT1</td>
+      </tr>
+      <tr>
+        <td>
+          <button onClick={this.buttonInput2}>IN2</button>
+        </td>
+        <td>S2</td>
+        <td>OUT2</td>
+      </tr>
+      <tr>
+        <td>
+          <button onClick={this.buttonInput3}>IN3</button>
+        </td>
+        <td>S3</td>
+        <td>OUT3</td>
+      </tr>
+    </tbody>
+  </table>
     </div>
   }
 }
+
 
 export default App;
